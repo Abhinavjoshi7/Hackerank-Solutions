87e89a94aad3589070740c0c4c7dd3c73e1fc922
Abhinavjoshi7 <92068770+Abhinavjoshi7@users.noreply.github.com>
2023-12-13 21:05:33 -0700

    string split and what's your name solution

---
+++ string-split-and-join.py
@@ -0,0 +1,6 @@
+def split_and_join(line):
+    # write your code here
+   line_list =  line.split()
+   line = "-".join(line_list)
+   return line
+
---
+++ whats-your-name.py
@@ -0,0 +1,16 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    first.strip()
+    last.strip()
+    str = last + "!"
+    # Write your code here
+    print("Hello", first, str, "You just delved into python.")
+
